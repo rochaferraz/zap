cd51634a599f7da35aff45e58a087e0e2657a61d
Vivien Nicolas <vnicolas@apple.com>
2021-12-02 12:14:05 +0100

    Use deviceType/deviceVersion from the endpoint table instead of hardcoding it in helper-endpointconfig.js

--- src-electron/generator/helper-endpointconfig.js
+++ src-electron/generator/helper-endpointconfig.js
@@ -437,8 +437,8 @@ async function collectAttributes(endpointTypes) {
     }
 
     let device = {
-      deviceId: 0,
-      deviceVersion: 1,
+      deviceId: ept.deviceIdentifier,
+      deviceVersion: ept.endpointVersion,
     }
     endpointAttributeSize = 0
     deviceList.push(device)
@@ -696,6 +696,8 @@ function endpoint_config(options) {
       let endpointTypeIds = []
       endpoints.forEach((ep) => {
         endpointTypeIds.push({
+          deviceIdentifier: ep.deviceIdentifier,
+          endpointVersion: ep.endpointVersion,
           endpointTypeId: ep.endpointTypeRef,
           endpointIdentifier: ep.endpointId,
         })
@@ -709,7 +711,9 @@ function endpoint_config(options) {
           queryEndpointType
             .selectEndpointType(db, eptId.endpointTypeId)
             .then((ept) => {
-              ept.endpointId = eptId.endpointIdentifier
+              ept.endpointId = eptId.endpointIdentifier;
+              ept.endpointVersion = eptId.endpointVersion;
+              ept.deviceIdentifier = eptId.deviceIdentifier;
               return ept
             })
         )
